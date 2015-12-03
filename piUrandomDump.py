b1a16f04a383c202c983176d9e4de44260972fad
RhiRabbit <rhiannon_e_davies@btinternet.com>
2015-12-03 22:59:43 +0000

    write bytes read to a randomly named file on external HD

--- piUrandomDump.py
+++ piUrandomDump.py
@@ -2,20 +2,27 @@
 #Then it dumps the N bytes at the end of file /home/students/rxd158/numbers
 #It will continue doing so until 1mb of numbers are dumped 
 
-import struct
+import random 
 
 chunk_size = 100
 current_read = 0;
-total_size = 1024 
+total_size = 1024 * 1024  
 output_folder = '/media/pi/Seagate Expansion Drive/'
+source_file = 'urandom'
 
 print 'Starting...'
-f=open('/dev/urandom')
+
+f=open('/dev/%s'%source_file)
+out = open(output_folder + source_file + str(random.random()),'w+'); 
 
 while current_read < total_size :
     current_read = current_read + chunk_size
     print 'Reading %d/%d (%f%%) bytes' % ( current_read, total_size , float(current_read)*100/float(total_size) )
     aux = f.read(chunk_size)
+    out.write(aux)
+    out.flush()
+
 
 print 'Finished'
 f.close()
+out.close()
