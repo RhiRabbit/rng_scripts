0b5338009ee810fa0dd81d0b7624b95046159a60
iceiony <iceiony@yahoo.com>
2015-12-11 23:44:54 +0000

    Add C code for reading from openssl RAND_bytes lib method

---
+++ opensshRandBytes.c
@@ -0,0 +1,27 @@
+#include <openssl/rand.h>
+#include <stdlib.h>             /* Standard Lib    ISOC  */
+#include <stdio.h>              /* I/O lib         ISOC  */
+
+int main(int argc, char *argv[]) {
+ unsigned int totalSize = 1024 * 1024 * 1024;
+ unsigned int buffSize = 100;
+
+ unsigned char buffer[buffSize];
+ do{
+
+   if( !(RAND_bytes((unsigned char *)buffer, sizeof(buffer)))) {
+     printf("ERROR: call to RAND_bytes() failed\n");
+     exit(1);
+   }
+
+   int index=0;
+   for(index = 0;index<buffSize;index++){
+     printf("%c", buffer[index]);
+   }
+   totalSize -= buffSize;
+ } 
+ while (totalSize > 0) ;
+
+ return 0;
+}
+
