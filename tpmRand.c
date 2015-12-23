449f57f9aafb306402280bdcd7d19e03b5ba0827
rhirabbit <rhiannon_e_davies@btinternet.com>
2015-12-23 20:47:08 +0000

    Fix tpmRand.c code to work and update readme with instructions

--- tpmRand.c
+++ tpmRand.c
@@ -1,89 +1,60 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<tss/tss error.h>
-#include<tss/platform.h>
-#include<tss/tss defines.h>
-#include<tss/tss typedef.h>
-#include<tss/tss structs.h>
-#include<tss/tspi.h>
-#include<trousers/trousers.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <tss/tss_error.h>
+#include <tss/platform.h>
+#include <tss/tss_defines.h>
+#include <tss/tss_typedef.h>
+#include <tss/tss_structs.h>
+#include <tss/tspi.h>
+#include <trousers/trousers.h>
+
+#define DBG(message, tResult) printf("Line%d, %s)%s returned 0x%08x.  %s.\n",__LINE__, __func__   ,message, tResult,(char *)Trspi_Error_String(tResult))
 
 int main(int argc,char **argv)
 {
-TSS HCONTEXT hContext;
-TSS HTPM hTPM;
-TSS RESULT result;
-TSS HKEY hSRK=0;
-TSS HPOLICY hSRKPolicy=0;
-TSS UUID SRK UUID=TSS UUID SRK;
-BYTE wks[2]; //For the well known secret
+    TSS_HCONTEXT hContext;
+    TSS_HTPM hTPM;
+    TSS_RESULT result;
 
-// Set wks to the well known secret: 20 bytes of all 0.s 
+    //setup TPM context and connect to TPM chip
+    result = Tspi_Context_Create( &hContext);
+    DBG("Create Context",result);
 
-memset(wks,0,20);
+    result = Tspi_Context_Connect(hContext, NULL);
+    DBG("Context Connect",result);
 
-//Pick the TPM you are talking to.
-// In this case, it is the system TPM (indicated with NULL).
-
-result = Tspi Context Create( &hContext);
-DBG(.Create Context.,result);
-
-result = Tspi Context Connect(hContext, NULL);
-DBG(.Context Connect.,result);
-
-// Get the TPM handle
-result=Tspi Context GetTpmObject(hContext,&hTPM);
-    DBG(.Get TPM Handle.,result);
-
-// Get the SRK handle
-result=Tspi Context LoadKeyByUUID(hContext,TSS PS TYPE SYSTEM,SRK UUID,&hSRK);
-    DBG(.Got the SRK handle., result);
-
-//Get the SRK policy
-result = Tspi GetPolicyObject(hSRK,TSS POLICY USAGE,&hSRKPolicy);
-    DBG(.Got the SRK policy.,result);
-
-//Then set the SRK policy to be the well known secret
-result=Tspi Policy SetSecret(hSRKPolicy,TSS SECRET MODE SHA1,20,wks);
-
-//Note: TSS SECRET MODE SHA1 says .Don.t hash this.
-// Use the 20 bytes as they are.
-
-    DBG(.Set the SRK secret in its policy.,result);
+    result=Tspi_Context_GetTpmObject(hContext,&hTPM);
+    DBG("Get TPM Handle",result);
 
 
-// MAIN BODY VARIABLES BYTE *randomBytes;
-FILE *fout;
-int i;
-int numRandomBytesOut=atoi(argv[1])
+    //read random bytes from tpm chip
+    int numRandomBytesOut = 100;
+    BYTE *randomBytes;
+    FILE *fout;
 
-
-
-
-// ENTER MAIN BODY
-
-
-if((randomBytes=(BYTE *)malloc(numRandomBytesOut))==NULL)
+    if((randomBytes=(BYTE *)malloc(numRandomBytesOut))==NULL)
     {
-	printf(.ERROR ALLOCATING randomBytes\n.);
-	return 1;
+    	printf("ERROR ALLOCATING randomBytes\n");
+    	return 1;
     }
-// Ask the TPM for a random number and put it in the random
-// Bytes Variable
-result=Tspi TPM GetRandom(hTPM,numRandomBytesOut,&randomBytes);
-  DBG(.Get Random number from TPM., result);
+    
+    // Ask the TPM for a random number and put it in the random
+    // Bytes Variable
+    result = Tspi_TPM_GetRandom(hTPM,numRandomBytesOut,&randomBytes);
+    DBG("Get Random number from TPM", result);
 
-fout = fopen(.random.out.,.wb.);
+    fout = fopen("random.out","wb");
     write(fileno(fout),	randomBytes,	numRandomBytesOut);
-fclose(fout);
+    fclose(fout);
 
-// Clean up
-Tspi Context Close(hobjects you have created);
-Tspi Context FreeMemory(hContext,NULL);
-// This frees up memory automatically allocated for you.
-Tspi Context Close(hContext);
-return 0;
+    // Clean up
+    Tspi_Context_Close(hTPM);
+    Tspi_Context_FreeMemory(hContext,NULL);
+    // This frees up memory automatically allocated for you.
+    Tspi_Context_Close(hContext);
+    return 0;
 }
